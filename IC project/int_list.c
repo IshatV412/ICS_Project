4e761dd18bcddc3e595acb92996e8cda1e922952
ishita-0301 <ishita03012005@gmail.com>
2025-04-14 00:01:45 +0530

    Update int_list.c

--- IC project/int_list.c
+++ IC project/int_list.c
@@ -1,190 +1,233 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
-#include <stdbool.h>
+#include <string.h>
+#include "intlist.h"
+#define MAX_LIST_NAME 50
+typedef struct {
+    char name[50];
+    int *value;
+    int size;
+    int capacity;
+    int scope;
+} intlist;
 
-#define MAX_LENGTH 50
+int intlist_capacity = 1;
+int intlist_size = 0;
+intlist *intlist_loc = NULL;
 
-// Structure to represent a dynamic list
-typedef struct{
-    char name[MAX_LENGTH]; // Name of the list
-    char dtype; // Data type (for future use, e.g., 'i' for int)
-    int size; // Size of the list
-    int *ptr; // Pointer to dynamically allocated array
-} List;
-
-// Function to initialize a List with a given size
-List* intlist_init(int arr_size) {
-    List* lst_ptr = (List*)malloc(sizeof(List));
-    lst_ptr->size = arr_size;
-    lst_ptr->ptr = (int*)malloc(arr_size * sizeof(int));
-    return lst_ptr;
+// Initialize storage
+void init_intlist_storage() {
+    intlist_loc = (intlist*) malloc(intlist_capacity * sizeof(intlist));
+    if (!intlist_loc) exit(1);
 }
 
-// Function to initialize all elements of an array with a given value
-int* intlist_initialize(int *ptr, int arr_size, int val) {
-    for (int i = 0; i < arr_size; i++) {
-        ptr[i] = val;
-    }
-    return ptr;
+// Resize storage
+void increase_intlist_capacity() {
+    intlist_capacity *= 2;
+    intlist *temp = (intlist*) realloc(intlist_loc, intlist_capacity * sizeof(intlist));
+    if (!temp) exit(1);
+    intlist_loc = temp;
 }
 
-// Function to create a clone of an array
-int* intlist_clone(int *ptr, int arr_size) {
-    int* new_ptr = (int*)malloc(arr_size * sizeof(int)); 
-    for (int i = 0; i < arr_size; i++) {
-        new_ptr[i] = ptr[i];
-    }
-    return new_ptr;
-}
-
-// Function to get the index of the first occurrence of a value
-int intlist_getfirst(int *ptr, int arr_size, int val) {
-    for (int i = 0; i < arr_size; i++) {
-        if (ptr[i] == val) {
-            return i;
+// Add a new list
+void add_intlist_variable(const char *name, int size, int init_val, int scope) {
+    for (int i = 0; i < intlist_size; i++) {
+        if (strcmp(intlist_loc[i].name, name) == 0 && intlist_loc[i].scope == scope) {
+            exit(1);
         }
     }
-    return -1; // Return -1 if value is not found
+
+    if (intlist_size == intlist_capacity) {
+        increase_intlist_capacity();
+    }
+
+    strncpy(intlist_loc[intlist_size].name, name, MAX_LIST_NAME - 1);
+    intlist_loc[intlist_size].name[MAX_LIST_NAME - 1] = '\0';
+
+    intlist_loc[intlist_size].value = (int*) malloc(size * sizeof(int));
+    for (int i = 0; i < size; i++) {
+        intlist_loc[intlist_size].value[i] = init_val;
+    }
+
+    intlist_loc[intlist_size].size = size;
+    intlist_loc[intlist_size].capacity = size;
+    intlist_loc[intlist_size].scope = scope;
+    intlist_size++;
 }
 
-// Function to get the index of the last occurrence of a value
-int intlist_getlast(int *ptr, int arr_size, int val) {
-    for (int i = arr_size - 1; i >= 0; i--) {
-        if (ptr[i] == val) {
-            return i;
+// Retrieve pointer by name and highest available scope
+intlist* get_intlist_variable(const char *name, int current_scope) {
+    intlist *result = NULL;
+    int highest_scope = -1;
+
+    for (int i = 0; i < intlist_size; i++) {
+        if (strcmp(intlist_loc[i].name, name) == 0 && intlist_loc[i].scope <= current_scope) {
+            if (intlist_loc[i].scope > highest_scope) {
+                highest_scope = intlist_loc[i].scope;
+                result = &intlist_loc[i];
+            }
         }
     }
-    return -1; // Return -1 if value is not found
+    return result;
 }
 
-// Function to insert an element at a given index
-void intlist_insert(List *lst, int index, int val) {
-    if (index < 0 || index > lst->size) return; // Check for valid index
+// Update value at index
+void update_intlist_element_ptr(intlist *lst, int index, int value) {
+    if (lst && index >= 0 && index < lst->size) {
+        lst->value[index] = value;
+    } else {
+        exit(1);
+    }
+}
+
+// Delete a variable
+void delete_intlist_variable_ptr(intlist *lst) {
+    if (!lst) exit(1);
+    int found = 0;
+
+    for (int i = 0; i < intlist_size; i++) {
+        if (&intlist_loc[i] == lst) found = 1;
+        if (found && i < intlist_size - 1) {
+            intlist_loc[i] = intlist_loc[i + 1];
+        }
+    }
+
+    if (found) intlist_size--;
+    else exit(1);
+}
+
+// Display all variables
+void display_intlist_variables() {
+    if (intlist_size == 0) {
+        printf("No int lists found.\n");
+        return;
+    }
+
+    for (int i = 0; i < intlist_size; i++) {
+        printf("Name: %s, Scope: %d, Values: ", intlist_loc[i].name, intlist_loc[i].scope);
+        for (int j = 0; j < intlist_loc[i].size; j++) {
+            printf("%d ", intlist_loc[i].value[j]);
+        }
+        printf("\n");
+    }
+}
+
+// Free all variables of a scope
+void free_intlist_variables_by_scope(int scope) {
+    int new_size = 0;
+    for (int i = 0; i < intlist_size; i++) {
+        if (intlist_loc[i].scope != scope) {
+            intlist_loc[new_size++] = intlist_loc[i];
+        } else {
+            free(intlist_loc[i].value);
+        }
+    }
+    intlist_size = new_size;
+}
+
+// Free all memory
+void free_all_intlist_memory() {
+    for (int i = 0; i < intlist_size; i++) {
+        free(intlist_loc[i].value);
+    }
+    free(intlist_loc);
+    intlist_loc = NULL;
+    intlist_capacity = 1;
+    intlist_size = 0;
+}
+
+//Utility Functions
+
+void intlist_insert(intlist *lst, int index, int val) {
+    if (!lst || index < 0 || index > lst->size) return;
+    lst->value = (int*) realloc(lst->value, (lst->size + 1) * sizeof(int));
+    for (int i = lst->size; i > index; i--) {
+        lst->value[i] = lst->value[i - 1];
+    }
+    lst->value[index] = val;
     lst->size++;
-    lst->ptr = (int*)realloc(lst->ptr, lst->size * sizeof(int));
-    for (int i = lst->size - 1; i > index; i--) {
-        lst->ptr[i] = lst->ptr[i - 1];
-    }
-    lst->ptr[index] = val;
 }
 
-// Function to delete an element at a given index
-void intlist_delete(List *lst, int index) {
-    if (index < 0 || index >= lst->size) return; // Check for valid index
+void intlist_delete(intlist *lst, int index) {
+    if (!lst || index < 0 || index >= lst->size) return;
     for (int i = index; i < lst->size - 1; i++) {
-        lst->ptr[i] = lst->ptr[i + 1];
+        lst->value[i] = lst->value[i + 1];
     }
     lst->size--;
-    lst->ptr = (int*)realloc(lst->ptr, lst->size * sizeof(int));
+    lst->value = (int*) realloc(lst->value, lst->size * sizeof(int));
 }
 
-// Function to sort an array using Bubble Sort
-void intlist_bubbleSort(int *ptr, int size) {
-    for (int i = 0; i < size - 1; i++) {
-        for (int j = 0; j < size - i - 1; j++) {
-            if (ptr[j] > ptr[j + 1]) {
-                int temp = ptr[j];
-                ptr[j] = ptr[j + 1];
-                ptr[j + 1] = temp;
+void intlist_remove_duplicates(intlist *lst) {
+    for (int i = 0; i < lst->size; i++) {
+        for (int j = i + 1; j < lst->size; j++) {
+            if (lst->value[i] == lst->value[j]) {
+                intlist_delete(lst, j);
+                j--;
             }
         }
     }
 }
 
-// Merge function for Merge Sort
-void intlist_merge(int *arr, int left, int mid, int right) {
-    int i, j, k;
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
-    int L[n1], R[n2];
-    for (i = 0; i < n1; i++) L[i] = arr[left + i];
-    for (j = 0; j < n2; j++) R[j] = arr[mid + 1 + j];
-    i = j = 0; k = left;
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) arr[k++] = L[i++];
-        else arr[k++] = R[j++];
+// Simple math/analytics
+int intlist_sum(int *arr, int size) {
+    int total = 0;
+    for (int i = 0; i < size; i++) total += arr[i];
+    return total;
+}
+
+int intlist_min(int *arr, int size) {
+    int min = arr[0];
+    for (int i = 1; i < size; i++) if (arr[i] < min) min = arr[i];
+    return min;
+}
+
+int intlist_max(int *arr, int size) {
+    int max = arr[0];
+    for (int i = 1; i < size; i++) if (arr[i] > max) max = arr[i];
+    return max;
+}
+
+float intlist_average(int *arr, int size) {
+    return (float)intlist_sum(arr, size) / size;
+}
+
+int intlist_getfirst(int *arr, int size, int val) {
+    for (int i = 0; i < size; i++) if (arr[i] == val) return i;
+    return -1;
+}
+
+int intlist_getlast(int *arr, int size, int val) {
+    for (int i = size - 1; i >= 0; i--) if (arr[i] == val) return i;
+    return -1;
+}
+
+void intlist_bubbleSort(int *arr, int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int t = arr[j]; arr[j] = arr[j + 1]; arr[j + 1] = t;
+            }
+        }
     }
+}
+
+void intlist_merge(int *arr, int left, int mid, int right) {
+    int n1 = mid - left + 1, n2 = right - mid;
+    int L[n1], R[n2];
+    for (int i = 0; i < n1; i++) L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++) R[j] = arr[mid + 1 + j];
+    int i = 0, j = 0, k = left;
+
+    while (i < n1 && j < n2) arr[k++] = (L[i] <= R[j]) ? L[i++] : R[j++];
     while (i < n1) arr[k++] = L[i++];
     while (j < n2) arr[k++] = R[j++];
 }
 
-// Function to sort an array using Merge Sort
 void intlist_mergeSort(int *arr, int left, int right) {
     if (left < right) {
-        int mid = left + (right - left) / 2;
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-        merge(arr, left, mid, right);
+        int mid = (left + right) / 2;
+        intlist_mergeSort(arr, left, mid);
+        intlist_mergeSort(arr, mid + 1, right);
+        intlist_merge(arr, left, mid, right);
     }
 }
-
-// Function to calculate the sum of elements in an array
-int intlist_sum(int *ptr, int size) {
-    int total = 0;
-    for (int i = 0; i < size; i++) total += ptr[i];
-    return total;
-}
-
-// Function to find the minimum element in an array
-int intlist_min(int *ptr, int size) {
-    int minimum = ptr[0];
-    for (int i = 0; i < size; i++) {
-        if (ptr[i] < minimum) minimum = ptr[i];
-    }
-    return minimum;
-}
-
-// Function to find the maximum element in an array
-int intlist_max(int *ptr, int size) {
-    int maximum = ptr[0];
-    for (int i = 0; i < size; i++) {
-        if (ptr[i] > maximum) maximum = ptr[i];
-    }
-    return maximum;
-}
-
-// Function to calculate the average of an array
-float intlist_average(int *ptr, int size) {
-    return (float)sum(ptr, size) / size;
-}
-
-// Function to remove duplicate elements from the list
-void intlist_removeDuplicates(List *lst) {
-    for(int i=0; i<lst->size; i++){
-        for (int j=i+1; j<lst->size; j++){
-            if ((lst->ptr)[j]==(lst->ptr)[i]){
-                for (int k=j; k< lst->size-1; k++){
-                    (lst->ptr)[k] = (lst->ptr)[k+1];
-                }
-                j--;
-                lst->size--;
-            }
-        }
-    }
-    lst->ptr = (int*)realloc(lst->ptr, lst->size*sizeof(int));
-    
-}
-
-// Function to display elements of an array
-void intlist_display(int *ptr, int size) {
-    for (int i = 0; i < size; i++) {
-        printf("%d ", ptr[i]);
-    }
-    printf("\n");
-}
-
-// int main() {
-//     // Test the implemented functionalities
-//     List* myList = init(5);
-//     initialize(myList->ptr, 5, 1);
-//     myList->ptr[2] = 3;
-//     myList->ptr[3] = 5;
-//     myList->ptr[4] = 3;
-//     removeDuplicates(myList);
-//     display(myList->ptr, myList->size);
-    
-//     free(myList->ptr);
-//     free(myList);
-//     return 0;
-// }
