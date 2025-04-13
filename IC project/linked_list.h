e27f7b23c9e29ad343bbe2304472671cd1f39ea9
Parrva-Shah <pcshah2006@gmail.com>
2025-04-14 01:09:43 +0530

    Update linked_list.h

--- IC project/linked_list.h
+++ IC project/linked_list.h
@@ -3,12 +3,10 @@
 
 #define MAX_NAME_LENGTH 50
 
-//structure for each node of linked list
 struct node {
     int value;
     struct node* ptr_next;
 };
-
 //struct for linked list 
 typedef struct {
     char name[50];
@@ -18,31 +16,24 @@ typedef struct {
     struct node* tail_ptr; //pointer to the tail node
 } linked_list;
 
-// Vector operations
-void initialise_ll_storage(linked_list** ll_storage,int* ll_capacity, int* ll_size);
-void increase_ll_capacity(linked_list** ll_storage, int* ll_capacity);
-void initialise_and_add_list(linked_list** ll_storage, int* ll_size, int* ll_capacity, char* name, int length, int scope);
+void initialise_ll_storage();
+void increase_ll_capacity();
 
-// List operations
-void insert_element(linked_list* list, int position, int value);
-void modify_value(linked_list* list, int position, int value);
-int read_node(linked_list* list, int position);
+linked_list* create_list(char* name, int length, int scope);
+linked_list* get_list_by_name_scope(char* name, int scope);
+
+void delete_list(linked_list* list);
+void insert_element(linked_list* list, int index, int value);
+void modify_value(linked_list* list, int index, int value);
+int read_node(linked_list* list, int index);
 void search_element(linked_list* list, int value);
 int get_First(linked_list* list, int value);
-
-// Deletion and cleanup
-void delete_node(linked_list* list, int position);
-void delete_list(linked_list* list,linked_list** ll_storage,int* ll_size);
+void delete_node(linked_list* list, int index);
 void empty_list(linked_list* list);
-
-// Sorting and display
 void sort_list(linked_list* list);
 void print_values(linked_list* list);
 void print_list(linked_list* list);
-linked_list* get_linked_list(linked_list** storage, int* size, char* name,int scope);
-
-
-// Test function
-void test_operations();
+void cleanup_all_lists();
+void test();
 
 #endif // LINKED_LIST_H
