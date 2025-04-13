ce37798525b9ab118afdbee62ede2d044a7f5217
Parrva-Shah <pcshah2006@gmail.com>
2025-04-13 22:35:39 +0530

    Update Integers.c

--- IC project/Integers.c
+++ IC project/Integers.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include "integers.h"
 
+
 // Function to initialize storage
 void int_init_storage(integer **int_loc, int *capacity, int *size) {
     *capacity = 1;
@@ -28,7 +29,7 @@ void int_increase_capacity(integer **int_loc, int *capacity) {
 // Add variable
 void int_add_variable(integer **int_loc, int *size, int *capacity, char *name, int value, int scope) {
     if (*size == *capacity) {
-        increase_capacity(int_loc, capacity);
+        int_increase_capacity(int_loc, capacity);
     }
     strcpy((*int_loc)[*size].name, name);
     (*int_loc)[*size].value = value;
@@ -44,8 +45,8 @@ integer* int_get_variable(integer **int_loc, int* size, char *name, int scope) {
     }
     integer* latest = NULL;
     for (int i = 0; i < *size; i++) {
-        if ((strcmp(int_loc[i]->name,name) == 0) && int_loc[i]->scope <= scope) {
-            if (latest == NULL || int_loc[i]->scope > latest->scope) {
+        if ((strcmp((*int_loc)[i].name,name) == 0) && (*int_loc)[i].scope <= scope) {
+            if (latest == NULL || (*int_loc)[i].scope > latest->scope) {
                 latest = int_loc[i];
             }
         }
@@ -81,7 +82,7 @@ int int_less_equals(int a, int b) { return a <= b; }
 // More integer functions
 int int_factorial(int n) { if (n < 0) return -1; int f = 1; for (int i = 1; i <= n; i++) f *= i; return f; }
 int int_gcd(int a, int b) { while (b != 0) { int t = b; b = a % b; a = t; } return a; }
-int int_lcm(int a, int b) { return (a * b) / gcd(a, b); }
+int int_lcm(int a, int b) { return (a * b) /int_gcd(a, b); }
 int int_is_prime(int n) { if (n < 2) return 0; for (int i = 2; i*i <= n; i++) if (n % i == 0) return 0; return 1; }
 int int_is_perfect_square(int n) { for (int i = 0; i*i <= n; i++) if (i*i == n) return 1; return 0; }
 int int_reverse_integer(int n) { int r = 0; while (n != 0) { r = r*10 + n%10; n /= 10; } return r; }
@@ -89,15 +90,9 @@ int int_count_digits(int n) { if (n == 0) return 1; int c = 0; while (n != 0) { 
 int int_sum_of_digits(int n) { int s = 0; while (n != 0) { s += n % 10; n /= 10; } return s; }
 
 // I/O and management
-void int_read_variable(integer **int_loc, int *size, int *capacity, char *name) {
-    int value;
-    printf("Enter value for %s: ", name);
-    scanf("%d", &value); 
-    add_variable(int_loc, size, capacity, name, value);
-}
 
-void int_print_variable(integer *int_loc, int size, char *name) {
-    integer *var = get_variable(int_loc, size, name);
+void int_print_variable(integer *int_loc, int size, char *name,int scope) {
+    integer *var = int_get_variable(&int_loc, &size, name,scope);
     if (var) {
         printf("%s = %d\n", var->name, var->value);
     } else {
@@ -105,8 +100,8 @@ void int_print_variable(integer *int_loc, int size, char *name) {
     }
 }
 
-void int_update_variable(integer *int_loc, int size, char *name, int new_value) {
-    integer *var = get_variable(int_loc, size, name);
+void int_update_variable(integer *int_loc, int size, char *name, int new_value,int scope) {
+    integer *var = int_get_variable(&int_loc, &size, name,scope);
     if (var) {
         var->value = new_value;
         printf("%s updated to %d\n", name, new_value);
@@ -164,38 +159,38 @@ void int_test() {
     integer *int_loc = NULL;
     int capacity = 0, size = 0;
 
-    init_integer_array(&int_loc, &capacity, &size);
+    int_init_storage(&int_loc, &capacity, &size);
 
-    add_variable(&int_loc, &size, &capacity, "a", 5, 1);
-    add_variable(&int_loc, &size, &capacity, "b", 10, 1);
-    display_variables(int_loc, size);
+    int_add_variable(&int_loc, &size, &capacity, "a", 5, 1);
+    int_add_variable(&int_loc, &size, &capacity, "b", 10, 1);
+    int_display_variables(int_loc, size);
 
     printf("\nReading a new variable:\n");
-    read_variable(&int_loc, &size, &capacity, "c", 2);
 
-    update_variable(int_loc, size, "a", 15);
-    print_variable(int_loc, size, "a");
+    int_update_variable(int_loc, size, "a", 15,0);
+    int_print_variable(int_loc, size, "a",0);
 
     printf("\nDeleting variable 'b'\n");
-    delete_variable(int_loc, &size, "b");
-    display_variables(int_loc, size);
+    int_delete_variable(int_loc, &size, "b");
+    int_display_variables(int_loc, size);
 
     printf("\nMath test on 'a' and 'c':\n");
-    integer *a = get_variable(int_loc, size, "a");
-    integer *c = get_variable(int_loc, size, "c");
+    integer *a = int_get_variable(&int_loc, &size, "a",1);
+    integer *c = int_get_variable(&int_loc, &size, "c",2);
     if (a && c) {
-        printf("Sum = %d\n", add(a->value, c->value));
-        printf("Product = %d\n", multiply(a->value, c->value));
-        printf("Power = %d\n", power(a->value, 2));
-        printf("GCD = %d\n", gcd(a->value, c->value));
+        printf("Sum = %d\n", int_add(a->value, c->value));
+        printf("Product = %d\n", int_multiply(a->value, c->value));
+        printf("Power = %d\n", int_power(a->value, 2));
+        printf("GCD = %d\n", int_gcd(a->value, c->value));
     }
 
     printf("\nFreeing scope 1 variables:\n");
-    free_scope_variables(int_loc, &size, 1);
-    display_variables(int_loc, size);
+    int_free_scope_variables(int_loc, &size, 1);
+    int_display_variables(int_loc, size);
 
     printf("\nFreeing all memory.\n");
-    free_all(&int_loc, &capacity, &size);
+    int_free_all(&int_loc, &capacity, &size);
 
     return;
 }
+
