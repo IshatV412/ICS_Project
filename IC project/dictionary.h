873fa7f6669096127b27c01693a6e6d9bdf34104
Parrva-Shah <pcshah2006@gmail.com>
2025-04-13 20:37:42 +0530

    Update dictionary.h

--- IC project/dictionary.h
+++ IC project/dictionary.h
@@ -22,7 +22,7 @@ void increase_len(dictionary_C_C *dict, int len);
 
 dictionary_C_C* create_dictionary_C_C(int len, char name[50],int *size, int* capacity, dictionary_C_C **dict_storage, int scope);
 
-dictionary_C_C* get_linked_list(dictionary_C_C** dict_storage, int* dict_size, char* name, int scope);
+dictionary_C_C* get_dict(dictionary_C_C** dict_storage, int* dict_size, char* name, int scope);
 
 void insert_C_C(dictionary_C_C *dict, char *key, char *value);
 
