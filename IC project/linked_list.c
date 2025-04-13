0b3ec09579a21ba43ca7877751d9f4df841dedf7
Parrva-Shah <pcshah2006@gmail.com>
2025-04-14 01:07:02 +0530

    Update linked_list.c

--- IC project/linked_list.c
+++ IC project/linked_list.c
@@ -3,41 +3,56 @@
 #include <string.h>
 #include "linked_list.h"
 
+struct node {
+    int value;
+    struct node* ptr_next;
+};
 
-void initialise_ll_storage(linked_list** ll_storage,int* ll_capacity, int* ll_size) {
-    *ll_capacity = 1;
-    *ll_size = 0;
-    *ll_storage = (linked_list*)malloc((*ll_capacity) * sizeof(linked_list));
-    if (*ll_storage == NULL) {
+//struct for linked list 
+typedef struct {
+    char name[50];
+    int length;
+    int scope;
+    struct node* head_ptr; //pointer to the head node
+    struct node* tail_ptr; //pointer to the tail node
+} linked_list;
+
+int LL_capacity;
+int LL_size;
+linked_list** LL_Loc;
+
+void initialise_ll_storage() {
+    LL_capacity = 1;
+    LL_size = 0;
+    LL_Loc = (linked_list**)malloc(LL_capacity * sizeof(linked_list*));
+    if (LL_Loc == NULL) {
         exit(1);
     }
 }
 
-void increase_ll_capacity(linked_list** ll_storage, int* ll_capacity) {
-    *ll_capacity *= 2;
-    linked_list* temp = realloc(*ll_storage,(*ll_capacity) * sizeof(linked_list));
+void increase_ll_capacity() {
+    LL_capacity *= 2;
+    linked_list** temp = realloc(LL_Loc, LL_capacity * sizeof(linked_list*));
     if (temp == NULL) {
         exit(1);
     }
-    *ll_storage = temp;
+    LL_Loc = temp;
 }
 
-
-
-
-void initialise_and_add_list(linked_list** ll_storage, int* ll_size, int* ll_capacity, char* name, int length, int scope) {
-    if (*ll_size == *ll_capacity) {
-        increase_ll_capacity(ll_storage, ll_capacity);
+linked_list* create_list(char* name, int length, int scope) {
+    if (LL_size == LL_capacity) {
+        increase_ll_capacity();
     }
 
     linked_list* list = (linked_list*)malloc(sizeof(linked_list));
     if (list == NULL) {
         printf("Memory Allocation Failed for linked list.\n");
-        return;
+        return NULL;
     }
 
     strncpy(list->name, name, sizeof(list->name) - 1);
     list->name[sizeof(list->name) - 1] = '\0';
+    list->scope = scope;
     list->length = 0;
     list->head_ptr = NULL;
     list->tail_ptr = NULL;
@@ -46,7 +61,7 @@ void initialise_and_add_list(linked_list** ll_storage, int* ll_size, int* ll_cap
         struct node* new_node = (struct node*)malloc(sizeof(struct node));
         if (new_node == NULL) {
             printf("Memory Allocation Failed for node.\n");
-            return;
+            return NULL;
         }
         new_node->value = 0;
         new_node->ptr_next = NULL;
@@ -61,216 +76,16 @@ void initialise_and_add_list(linked_list** ll_storage, int* ll_size, int* ll_cap
         list->length++;
     }
 
-    (ll_storage)[*ll_size] = list;
-    (*ll_size)++;
+    LL_Loc[LL_size++] = list;
+    return list;
 }
 
-//function to insert element at a given position
-void insert_element(linked_list* list, int position, int value) {
-    if (position < 0 || position > list->length) { //validating position
-        printf("Invalid Position");
-        return;
-    }
-    struct node* new_node = (struct node*)malloc(sizeof(struct node)); //creating the new node
-    if (new_node == NULL) {
-        printf("Memory Allocation Failed");
-        return;
-    }
-
-    new_node->value = value;
-    new_node->ptr_next = NULL;
-    if (position == 0) { //if to be added at the beginning change the head pointer as well
-        new_node->ptr_next = list->head_ptr;
-        list->head_ptr = new_node;
-        list->length = list->length + 1;
-        return;
-    }
-    else if (position == list->length) { //if to be added at the end change the tail pointer
-        list->tail_ptr->ptr_next = new_node;
-        list->tail_ptr = new_node;
-        list->length = list->length + 1;
-        return;
-    }
-    else {
-        struct node* temp = list->head_ptr; //temp struct to traverse through the list to get to the position needed
-        for (int i = 0; i < position - 1 && temp!=NULL; i++) {
-            temp = temp->ptr_next;
-        }
-        new_node->ptr_next = temp->ptr_next;
-        temp->ptr_next = new_node;
-        list->length = list->length + 1;
-        return;
-    }
-}
-//function to modify the value at a given position
-void modify_value(linked_list* list, int position, int value) {
-    
-    if (position < 0 || position >= list->length) { //validating position
-        printf("Invalid Position");
-        return;
-    }
-    
-    struct node* temp = list->head_ptr; //temp struct to traverse through the linked list
-    for (int i = 0; i < position; i++) {
-        temp = temp->ptr_next;
-    }
-    temp->value = value;
-    return;
-}
-//function to get the value at a given position
-int read_node(linked_list* list, int position) {
-    if (position<0 || position >= list->length) {
-        printf("Invalid Position");
-        return -1;
-    }
-
-    struct node* temp = list->head_ptr;
-    for (int i = 0; i < position; i++) {
-        temp = temp->ptr_next;
-    }
-    return temp->value;
-}
-
-void search_element(linked_list* list, int value) {
-    struct node* temp = list->head_ptr;
-    while(temp!=NULL) {
-        if (value == temp->value) {
-            printf("Element Found.\n");
-            return;
-        }
-        temp = temp->ptr_next;
-    }
-    printf("Element not found.\n");
-    return;
-}
-
-int get_First(linked_list* list, int value) {
-    struct node* temp = list->head_ptr;
-    int index = 0;
-    while(temp!=NULL) {
-        if (value == temp->value) {
-            return index;
-        }
-        temp = temp->ptr_next;
-        index++;
-    }
-    printf("Element not in list.\n");
-    return -1;
-}
-
-//function to print the entire linked list
-void print_values(linked_list* list) {
-    struct node* temp = list->head_ptr;
-    for (int i = 0; i < list->length; i++) {
-        printf("%d -> ",temp->value);
-        temp = temp->ptr_next;
-    }
-    printf("\n\n");
-    return;
-}
-
-//function to print all the details of the list
-void print_list(linked_list* list) {
-    printf ("%s %d\n",list->name,list->length);
-    print_values(list);
-}
-
-void sort_list(linked_list* list) {
-    struct node* current = list->head_ptr;
-    struct node* index = NULL;
-    while (current != NULL) { //tried bubble sort kinda thing, it is not bubble sort but it works lol its some random sort method
-        index = current->ptr_next;
-        while (index != NULL) {
-            if (current->value > index->value) {
-                int temp = current->value;
-                current->value = index->value;
-                index->value = temp;
-            }
-            index = index->ptr_next;
-        }
-        current = current->ptr_next;
-    }
-}
-
-//function to delete a specific node
-void delete_node(linked_list* list,int position) {
-    if (position < 0 || position >= list->length) {
-        printf("Invalid position");
-        return;
-    }
-    struct node* temp = list->head_ptr;
-
-    if (position == 0) {
-        list->head_ptr = temp->ptr_next;
-        list->length = list->length - 1;
-        if (list->length == 0) {
-            list->tail_ptr = NULL;
-        }
-        free(temp);
-        return;
-    }
-    for (int i = 0; i < position - 1; i++) {
-        temp = temp->ptr_next;
-    }
-    struct node* delete_node = temp->ptr_next;
-    temp->ptr_next = delete_node->ptr_next;
-
-    if (delete_node->ptr_next == list->tail_ptr) {
-        list->tail_ptr = temp;
-    }
-    list->length = list->length - 1;
-    free(delete_node);
-}
-//function to delete the list itself
-void delete_list(linked_list* list,linked_list** ll_storage,int* ll_size) {
-    if (list == NULL) {
-        return;
-    }
-    struct node* temp = list->head_ptr;
-    //freeing all the nodes of the list
-    while(temp!=NULL) {
-        struct node* next = temp->ptr_next;
-        free(temp);
-        temp = next;
-    }
-    for (int i = 0; i < *ll_size; i++) {
-        if (ll_storage[i] == list) {
-            for (int j = i; j < *ll_size - 1; j++) {
-                ll_storage[j] = ll_storage[j + 1];
-            }
-            (*ll_size)--;
-            break;
-        }
-    }
-    free(list);
-}
-
-void empty_list(linked_list* list) {
-    if (list == NULL) {
-        return;
-    }
-    struct node* temp = list->head_ptr;
-    //freeing all the nodes of the list
-    while(temp!=NULL) {
-        struct node* next = temp->ptr_next;
-        free(temp);
-        temp = next;
-    }
-    list->head_ptr = NULL;
-    list->tail_ptr = NULL;
-    list->length = 0;
-}
-linked_list* get_linked_list(linked_list** ll_storage, int* ll_size, char* name, int scope) {
-    if (ll_size == NULL || name == NULL) {
-        printf("Invalid storage or name.\n");
-        return NULL;
-    }
+linked_list* get_list_by_name_scope(char* name, int scope) {
     linked_list* latest = NULL;
-
-    for (int i = 0; i < *ll_size; i++) {
-        if ((strcmp(ll_storage[i]->name, name) == 0) && ll_storage[i]->scope <= scope ) {
-            if (latest == NULL || ll_storage[i]->scope > latest->scope) {
-                latest = ll_storage[i];
+    for (int i = 0; i < LL_size; i++) {
+        if (strcmp(LL_Loc[i]->name, name) == 0 && LL_Loc[i]->scope <= scope) {
+            if (latest == NULL || LL_Loc[i]->scope > latest->scope) {
+                latest = LL_Loc[i];
             }
         }
     }
@@ -280,45 +95,227 @@ linked_list* get_linked_list(linked_list** ll_storage, int* ll_size, char* name,
     return latest;
 }
 
+void delete_list(linked_list* list) {
+    if (list == NULL) return;
 
-void test_operations() {
-    linked_list** ll_storage;
-    int capacity, size;
-    initialise_ll_storage(ll_storage, &capacity, &size);
-
-    initialise_and_add_list(ll_storage, &size, &capacity, "List1", 5,0);
-    initialise_and_add_list(ll_storage, &size, &capacity, "List2", 3,1);
-
-    for (int i = 0; i < size; i++) {
-        print_list(ll_storage[i]);
+    struct node* temp = list->head_ptr;
+    while (temp != NULL) {
+        struct node* next = temp->ptr_next;
+        free(temp);
+        temp = next;
     }
 
-    insert_element(ll_storage[0], 2, 99);
-    insert_element(ll_storage[1], 1, 42);
-    modify_value(ll_storage[0], 3, 88);
-    modify_value(ll_storage[1], 0, 77);
-
-    for (int i = 0; i < size; i++) {
-        print_list(ll_storage[i]);
+    for (int i = 0; i < LL_size; i++) {
+        if (LL_Loc[i] == list) {
+            for (int j = i; j < LL_size - 1; j++) {
+                LL_Loc[j] = LL_Loc[j + 1];
+            }
+            LL_size--;
+            break;
+        }
+    }
+    free(list);
+}
+void insert_element(linked_list* list, int index, int value) {
+    if (list == NULL || index < 0 || index > list->length) {
+        printf("Invalid list or index for insertion.\n");
+        return;
     }
 
-    sort_list(ll_storage[0]);
-    sort_list(ll_storage[1]);
-    for (int i = 0; i < size; i++) {
-        print_list(ll_storage[i]);
+    struct node* new_node = (struct node*)malloc(sizeof(struct node));
+    new_node->value = value;
+    new_node->ptr_next = NULL;
+
+    if (index == 0) {
+        new_node->ptr_next = list->head_ptr;
+        list->head_ptr = new_node;
+        if (list->length == 0) list->tail_ptr = new_node;
+    } else {
+        struct node* temp = list->head_ptr;
+        for (int i = 0; i < index - 1; i++) temp = temp->ptr_next;
+        new_node->ptr_next = temp->ptr_next;
+        temp->ptr_next = new_node;
+        if (new_node->ptr_next == NULL) list->tail_ptr = new_node;
+    }
+    list->length++;
+}
+void modify_value(linked_list* list, int index, int value) {
+    if (list == NULL || index < 0 || index >= list->length) {
+        printf("Invalid index for modification.\n");
+        return;
+    }
+    struct node* temp = list->head_ptr;
+    for (int i = 0; i < index; i++) temp = temp->ptr_next;
+    temp->value = value;
+}
+int read_node(linked_list* list, int index) {
+    if (list == NULL || index < 0 || index >= list->length) {
+        printf("Invalid index for reading.\n");
+        return -1;
+    }
+    struct node* temp = list->head_ptr;
+    for (int i = 0; i < index; i++) temp = temp->ptr_next;
+    return temp->value;
+}
+void search_element(linked_list* list, int value) {
+    if (list == NULL) return;
+
+    struct node* temp = list->head_ptr;
+    int index = 0, found = 0;
+
+    while (temp != NULL) {
+        if (temp->value == value) {
+            printf("Value %d found at index %d\n", value, index);
+            found = 1;
+        }
+        temp = temp->ptr_next;
+        index++;
     }
 
-    delete_node(ll_storage[0], 2);
-    print_list(ll_storage[0]);
+    if (!found) {
+        printf("Value %d not found in the list.\n", value);
+    }
+}
+int get_First(linked_list* list, int value) {
+    if (list == NULL) return -1;
 
-    delete_list(ll_storage, &size, ll_storage[1]);
-    printf("List2 deleted.\n");
+    struct node* temp = list->head_ptr;
+    int index = 0;
 
-    delete_list(ll_storage, &size, ll_storage[0]);
-    free(ll_storage);
-    printf("Memory freed and program terminated.\n");
+    while (temp != NULL) {
+        if (temp->value == value) return index;
+        temp = temp->ptr_next;
+        index++;
+    }
+    return -1;
+}
+void delete_node(linked_list* list, int index) {
+    if (list == NULL || index < 0 || index >= list->length) {
+        printf("Invalid index for deletion.\n");
+        return;
+    }
+
+    struct node* to_delete;
+    if (index == 0) {
+        to_delete = list->head_ptr;
+        list->head_ptr = to_delete->ptr_next;
+        if (list->length == 1) list->tail_ptr = NULL;
+    } else {
+        struct node* temp = list->head_ptr;
+        for (int i = 0; i < index - 1; i++) temp = temp->ptr_next;
+        to_delete = temp->ptr_next;
+        temp->ptr_next = to_delete->ptr_next;
+        if (temp->ptr_next == NULL) list->tail_ptr = temp;
+    }
+    free(to_delete);
+    list->length--;
+}
+void empty_list(linked_list* list) {
+    if (list == NULL) return;
+
+    struct node* temp = list->head_ptr;
+    while (temp != NULL) {
+        struct node* next = temp->ptr_next;
+        free(temp);
+        temp = next;
+    }
+    list->head_ptr = NULL;
+    list->tail_ptr = NULL;
+    list->length = 0;
+}
+void sort_list(linked_list* list) {
+    if (list == NULL || list->length < 2) return;
+
+    for (struct node* i = list->head_ptr; i != NULL; i = i->ptr_next) {
+        for (struct node* j = i->ptr_next; j != NULL; j = j->ptr_next) {
+            if (i->value > j->value) {
+                int temp = i->value;
+                i->value = j->value;
+                j->value = temp;
+            }
+        }
+    }
+}
+void print_values(linked_list* list) {
+    if (list == NULL) return;
+
+    struct node* temp = list->head_ptr;
+    printf("[ ");
+    while (temp != NULL) {
+        printf("%d ", temp->value);
+        temp = temp->ptr_next;
+    }
+    printf("]");
+}
+void print_list(linked_list* list) {
+    if (list == NULL) {
+        printf("NULL list.\n");
+        return;
+    }
+
+    printf("List \"%s\" (Scope: %d, Length: %d): ", list->name, list->scope, list->length);
+    print_values(list);
+    printf("\n");
 }
 
+void cleanup_all_lists() {
+    for (int i = 0; i < LL_size; i++) {
+        delete_list(LL_Loc[i]);
+    }
+    free(LL_Loc);
+    LL_Loc = NULL;
+    LL_capacity = 0;
+    LL_size = 0;
+}
+void test() {
+    // Step 1: Initialize the LL_Loc system
+    initialise_ll_storage();
 
-//Coded by Parrva Chirag Shah (B24CS1053)
+    // Step 2: Create two lists with different scopes
+    linked_list* list1 = create_list("MyList", 4, 0);
+    linked_list* list2 = create_list("MyList", 3, 2); // Same name, different scope
 
+    // Step 3: Print their initial contents
+    printf("Initial Lists:\n");
+    print_list(list1);
+    print_list(list2);
+
+    // Step 4: Insert new elements into both lists
+    insert_element(list1, 2, 42);
+    insert_element(list2, 1, 77);
+
+    // Step 5: Modify existing values
+    modify_value(list1, 0, 11);
+    modify_value(list2, 2, 88);
+
+    // Step 6: Sort lists
+    sort_list(list1);
+    sort_list(list2);
+
+    // Step 7: Print modified lists
+    printf("\nAfter Insertions, Modifications & Sorting:\n");
+    print_list(list1);
+    print_list(list2);
+
+    // Step 8: Search and get first occurrence
+    printf("\nSearching 42 in list1:\n");
+    search_element(list1, 42);
+    printf("Index of 42: %d\n", get_First(list1, 42));
+
+    // Step 9: Retrieve list by name & scope
+    linked_list* latest_list = get_list_by_name_scope("MyList", 1); // Should return list1
+    if (latest_list != NULL) {
+        printf("\nList retrieved by name & scope:\n");
+        print_list(latest_list);
+    }
+
+    // Step 10: Delete one list
+    printf("\nDeleting list2...\n");
+    delete_list(list2);
+
+    // Step 11: Cleanup all lists
+    cleanup_all_lists();
+    printf("\nAll memory freed. Program ended successfully.\n");
+
+    return;
+}
