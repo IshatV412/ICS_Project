ed49580940ada2d6559a77d392c98ea3817cbf72
ishita-0301 <ishita03012005@gmail.com>
2025-04-14 02:03:17 +0530

    Update boolean.h

--- IC project/boolean.h
+++ IC project/boolean.h
@@ -11,27 +11,31 @@ struct boolean {
     int value;
     int scope;
 };
-
-
-void bool_init_storage(); 
+// Initialize boolean variable storage
+void bool_init_storage();
+// Increase storage capacity
 void bool_increase_capacity();
+// Add a new boolean variable (ensures unique name in scope)
 void bool_add_variable(char *name, int value, int scope);
-struct boolean* bool_get_variable(char *name, struct boolean *bool_loc);
+boolean* bool_get_variable(char *name, int scope);
+// Logical operations
+bool bool_or(bool a, bool b);  
+bool bool_and(bool a, bool b);  
+bool bool_not(bool a);     
+bool bool_nor(bool a, bool b);  
+bool bool_nand(bool a, bool b); 
+bool bool_xor(bool a, bool b);  
+bool bool_xnor(bool a, bool b); 
 
-bool bool_or(bool a, bool b) { return (a || b); }
-bool bool_and(bool a, bool b) { return (a && b); }
-bool bool_not(bool a) { return (!a); }
-bool bool_nor(bool a, bool b) { return (!(a || b)); }
-bool bool_nand(bool a, bool b) { return !(a && b); }
-bool bool_xor(bool a, bool b) { return (((!a) && b) || ((!b) && a)); }
-bool bool_xnor(bool a, bool b) { return !xor(a, b); }
-
-void bool_read_variable(char *name, int scope, struct boolean *bool_loc);
-void bool_print_variable(char *name, struct boolean *bool_loc);
-void bool_update_variable(char *name, int new_value, struct boolean *bool_loc);
-void bool_delete_variable(char *name, struct boolean *bool_loc);
-void bool_free_variables(int scope, struct boolean *bool_loc);
-void bool_free_all_memory(struct boolean *bool_loc);
-void bool_display_variables(struct boolean *bool_loc);
-
+// Update existing variable's value
+void bool_update_variable(char *name, int scope, int new_value);
+// Delete a boolean variable by name and scope
+void bool_delete_variable(char *name, int scope);
+// Free all variables with a specific scope
+void bool_free_scope_variables(int scope);
+// Free all allocated memory
+void bool_free_all_memory();
+// Display all boolean variables
+void bool_display_variables();
+void bool_test_all();
 #endif // BOOLEAN_H
