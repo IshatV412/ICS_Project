06183dcc62fb685814c2a46877db495a749c4da5
Parrva-Shah <pcshah2006@gmail.com>
2025-04-14 00:22:13 +0530

    Update integers.h

--- IC project/integers.h
+++ IC project/integers.h
@@ -2,26 +2,25 @@
 #define INTEGERS_H
 
 typedef struct {
-    char name[50];
-    int value;
-    int scope;
-} integer;
+    char int_name[50];
+    int int_value;
+    int int_scope;
+} int_var;
 
-// Storage management
-void int_init_storage(integer **int_loc, int *capacity, int *size);
-void int_increase_capacity(integer **int_loc, int *capacity);
-void int_free_all(integer **int_loc, int *capacity, int *size);
-void int_free_scope_variables(integer *int_loc, int *size, int scope);
 
-// Variable operations
-void int_add_variable(integer **int_loc, int *size, int *capacity, char *name, int value, int scope);
-integer* int_get_variable(integer **int_loc, int* size, char *name,int scope);
-void int_print_variable(integer *int_loc, int size, char *name,int scope);
-void int_update_variable(integer *int_loc, int size, char *name, int new_value,int scope);
-void int_delete_variable(integer *int_loc, int *size, char *name);
-void int_display_variables(integer *int_loc, int size);
+void int_init_storage();
+void int_increase_capacity();
+int_var* int_get_var(char *name, int scope);
+int_var* int_get_var_by_name(char *name);
+void int_add_variable(int_var *new_var);
+void int_read_variable(int_var *var);
+void int_update_variable(int_var *var, int new_value);
+void int_print_variable(int_var *var);
+void int_delete_variable(int_var *var);
+void int_free_scope(int scope);
+void int_free_all();
+void int_display_variables();
 
-// Arithmetic functions
 int int_add(int a, int b);
 int int_subtract(int a, int b);
 int int_multiply(int a, int b);
@@ -30,29 +29,24 @@ int int_modulus(int a, int b);
 int int_power(int a, int b);
 int int_absolute(int a);
 
-// Logical operations
-int int_logical_and(int a, int b);
-int int_logical_or(int a, int b);
-int int_logical_not(int a);
+int int_and(int a, int b);
+int int_or(int a, int b);
+int int_not(int a);
 
-// Relational operators
 int int_equals(int a, int b);
 int int_not_equals(int a, int b);
-int int_greater_than(int a, int b);
-int int_less_than(int a, int b);
-int int_greater_equals(int a, int b);
-int int_less_equals(int a, int b);
+int int_greater(int a, int b);
+int int_less(int a, int b);
+int int_greater_eq(int a, int b);
+int int_less_eq(int a, int b);
 
-// Utility functions
 int int_factorial(int n);
 int int_gcd(int a, int b);
 int int_lcm(int a, int b);
 int int_is_prime(int n);
-int int_is_perfect_square(int n);
-int int_reverse_integer(int n);
-int int_count_digits(int n);
-int int_sum_of_digits(int n);
-
-void int_test();
+int int_is_square(int n);
+int int_reverse(int n);
+int int_digit_count(int n);
+int int_digit_sum(int n);
 
 #endif // INTEGERS_H
