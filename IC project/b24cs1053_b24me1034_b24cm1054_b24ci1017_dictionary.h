890d58093f66ada5b9da80a93d08a5ea6401a90d
Parrva-Shah <pcshah2006@gmail.com>
2025-04-14 05:26:47 +0530

    Update b24cs1053_b24me1034_b24cm1054_b24ci1017_dictionary.h

--- IC project/b24cs1053_b24me1034_b24cm1054_b24ci1017_dictionary.h
+++ IC project/b24cs1053_b24me1034_b24cm1054_b24ci1017_dictionary.h
@@ -14,17 +14,19 @@ typedef struct{
     node_C_C *start; //stores the address of the first location of the dictionary
 }dictionary_C_C;
 
-void initialise_dict_storage(dictionary_C_C** dict_storage,int* dict_capacity, int* dict_size); 
-void increase_dict_capacity(dictionary_C_C** dict_storage, int* capacity);
+
+void initialise_dict_storage();
+void increase_dict_capacity();
+dictionary_C_C* get_dict( char* name, int scope);
 void increase_len(dictionary_C_C *dict, int len);
-dictionary_C_C* create_dictionary_C_C(int len, char name[50],int *size, int* capacity, dictionary_C_C **dict_storage, int scope);
-dictionary_C_C* get_dict(dictionary_C_C** dict_storage, int* dict_size, char* name, int scope);
+dictionary_C_C* create_dictionary_C_C(int len, char name[50], int scope);
 void insert_C_C(dictionary_C_C *dict, char *key, char *value);
 char* getvalue_C_C(dictionary_C_C *dict, char *key);
 char* getkey_C_C(dictionary_C_C *dict, char *key);
 void free_dict_C_C(dictionary_C_C *dict);
 void printing_C_C(dictionary_C_C *dict);
 int delete_C_C(dictionary_C_C *dict, char *key);
+void free_dict_storage(int scope);
 void test_dict_C_C();
 
 #endif
